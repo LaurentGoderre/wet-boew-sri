abdd8e83a66905a7d9c8e7d344b41c127bdb2b4a
Laurent Goderre <laurent.goderre@gmail.com>
2016-12-05 15:10:48 -0500

    Added the themes logic

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,21 @@ module.exports = function (grunt) {
                 pretty: true
             }
         },
-        sriDefaults = {
+        sriWetDefaults = {
             expand: true,
             src: [
-                "{js,css}/*.{js,css}"
+                "{js,css}/**/*.{js,css}"
+            ]
+        },
+        sriThemesDefaults = {
+            expand: true,
+            src: [
+                "{js,css}/*.{js,css}",
+                "!**/*wet-boew*",
+                "!**/noscript*",
+                "!deps/",
+                "!i18n/",
+                "polyfills/",
             ]
         },
         versions = grunt.file.expand('*-dist-*'),
@@ -19,36 +30,50 @@ module.exports = function (grunt) {
     for (v = 0; v < versionsLength; v+= 1) {
         version = versions[v];
 
-        versionMatch = version.match(/(.*)-dist-4\.0\.(\d*)/);
+        versionMatch = version.match(/(.*)-dist-4\.0\.(\d*)(?:-\d)?(?:-(theme-.*|gcweb))?/);
 
         if (versionMatch) {
             patchVersion = parseInt(versionMatch[2], 10);
 
-            if (versionMatch[1] = 'wet-boew' && patchVersion < 15) {
-                sriConfig[version] = Object.assign({}, sriDefaults,  {
+            if (versionMatch[1] === 'wet-boew' && patchVersion < 15) {
+                sriConfig[version] = Object.assign({}, sriWetDefaults,  {
                     options: {
                         dest: version + '/payload.json'
                     },
                     cwd: version
                 });
-            } else if ( versionMatch[1] = 'wet-boew' && patchVersion >= 15 ) {
-                cwd = version + '/wet-boew/'
-                sriConfig[version + "_core"] = Object.assign({}, sriDefaults,  {
+            } else if ( versionMatch[1] === 'wet-boew' && patchVersion >= 15 ) {
+                cwd = version + '/wet-boew/';
+                sriConfig[version + "_core"] = Object.assign({}, sriWetDefaults,  {
                     options: {
                         dest: cwd + '/payload.json'
                     },
                     cwd: cwd
                 });
-                cwd = version + '/theme-wet-boew/'
-                sriConfig[version + "_theme"] = Object.assign({}, sriDefaults,  {
+
+                cwd = version + '/theme-wet-boew/';
+                sriConfig[version + "_theme"] = Object.assign({}, sriThemesDefaults,  {
+                    options: {
+                        dest: cwd + '/payload.json'
+                    },
+                    cwd: cwd
+                });
+            } else if (versionMatch[1] === 'themes' && patchVersion < 15) {
+                sriConfig[version] = Object.assign({}, sriThemesDefaults,  {
+                    options: {
+                        dest: version + '/payload.json'
+                    },
+                    cwd: version
+                });
+            } else if (versionMatch[1] === 'themes' && patchVersion >= 15) {
+                cwd = version + '/' + versionMatch[3].replace('gcweb', 'GCWeb');
+                sriConfig[version] = Object.assign({}, sriThemesDefaults,  {
                     options: {
                         dest: cwd + '/payload.json'
                     },
                     cwd: cwd
                 });
             }
-
-
         }
     }
 
